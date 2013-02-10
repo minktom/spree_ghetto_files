366560404190ce369c31f57d90b46625d1a56db3
Tamas Drahos <drahostamas@gmail.com>
2013-02-10 22:34:27 +0100

    chosen, paginate

--- app/controllers/spree/admin/downloadables_controller.rb
+++ app/controllers/spree/admin/downloadables_controller.rb
@@ -1,2 +1,6 @@
 class Spree::Admin::DownloadablesController < Spree::Admin::ResourceController
+
+  def index
+    @downloadables = @downloadables.page(params[:page]).per(Spree::Config[:admin_products_per_page])
+  end
 end
